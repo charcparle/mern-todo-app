402e67e5a1c4af7bceab1ce230a624524edb1709
Charles Cheng <charlescheng@Charless-MacBook-Pro.local>
2021-03-21 13:22:51 +0800

    replaced with template code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+      
+    );
+  }
+}
+
+export default App;
+
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +41,4 @@ function App() {
 }
 
 export default App;
+*?
