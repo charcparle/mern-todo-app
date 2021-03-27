e36686fb7fcb28031582a86911cd985095683075
Charles Cheng <charlescheng@Charless-MacBook-Pro.local>
2021-03-27 15:42:41 +0800

    Added and wrapped with router tag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      
+      <router>
         <div className="container">
           <h2>MERN-Stack Todo App</h2>
         </div>
-      
+        </router>
     );
   }
 }
