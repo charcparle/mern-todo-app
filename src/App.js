f628d8d438ec03f56221f590b3959f6ddc6e7de7
Charles Cheng <charles.yhcheng@gmail.com>
2021-04-03 17:04:09 +0800

    Updated CreateTodo with forms and states

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import {CreateTodo} from "./components/create-todo.component";
+import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
 
